e3309edf6839904db512ac413912df1f6aaad4d7
aemerick <aemerick11@gmail.com>
2015-02-01 16:50:52 -0500

    Added Gato temperature lines to halo plot

--- analysis/halo/halo_plot.py
+++ analysis/halo/halo_plot.py
@@ -6,9 +6,11 @@ import cgs    as cgs # cgs unit conversions
 lw = 1.7
 # plot density and temperature profiles for 
 # things
-r = np.linspace(1.0, 250.0, 1000.0) * cgs.kpc
+r = np.linspace(1.0, 400.0, 1000.0) * cgs.kpc
 
 n = {'miller': gal.halo_gas_ndensity(r),
+     'miller_adj': gal.halo_gas_ndensity(r,n0=(0.46+0.74),rc=(0.35+0.29)*cgs.kpc,
+                                           beta=(0.71-0.14),ambient=0.0),
      'isothermal': gal.gato_ndensity(r, 'isothermal'),
      'adiabatic' : gal.gato_ndensity(r, 'adiabatic'),
      'cooling'   : gal.gato_ndensity(r, 'cooling'),
@@ -29,7 +31,7 @@ fig = plt.figure(figsize=[12,6])
 
 ax1 = fig.add_subplot(121)
 ax1.plot(r, n['miller'], label = 'Miller and Bregman 2013', color ='black',lw=lw)
-
+ax1.plot(r, n['miller_adj'], label = 'Adjusted MB2013', color ='black',lw=lw,ls='--')
 ax1.plot(r, n['isothermal'], label = "Gato Isothermal",
                                               lw = lw, color = 'blue',
                                               ls = '-')
@@ -44,12 +46,13 @@ ax1.plot(gal.kaufmann('01','r'), n['kaufmann01'], label = 'Kaufmann NFW',
                                            color = 'purple', lw = lw)
 ax1.plot(gal.kaufmann('02','r'), n['kaufmann02'], label = 'Kaufmann Realistic',
                                            color = 'purple', lw=lw, ls='--')
+
                                            
 ax1.semilogy()
 ax1.legend(loc='best')
 ax1.set_xlabel(r'r (kpc)')
 ax1.set_ylabel(r'n (cm$^{-3}$)')
-ax1.set_xlim(1.0,250.0)                                              
+ax1.set_xlim(1.0,np.max(r))                                              
 ax2 = fig.add_subplot(122)
 
 
@@ -72,8 +75,14 @@ ax2.plot(gal.kaufmann('02','r'), T['kaufmann02'], label = 'Kaufmann Realistic',
 ax2.semilogy()
 ax2.set_xlabel(r'r (kpc)')
 ax2.set_ylabel(r'T (K)')
+#ax2.legend(loc='best')
+ax2.set_xlim(1.0,np.max(r))                                              
+
+ax2.plot([50.0,90.0],[1.8E6,1.8E6],label=r'Gato T$_{min}$ - T$_{max}$',color='blue',
+                                  lw=lw, ls='-')
+ax2.plot([50.0,90.0],[3.0E6,3.0E6],color='blue',
+                                  lw=lw, ls='-')
 ax2.legend(loc='best')
-ax2.set_xlim(1.0,250.0)                                              
 plt.savefig('n_T_profile.png')
 plt.close()                                              
                                              
