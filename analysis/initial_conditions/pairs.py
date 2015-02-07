5e34efff64d6d1ad51049d126cbc2d8943dc8003
aemerick <aemerick11@gmail.com>
2015-02-07 18:22:25 -0500

    update

--- analysis/initial_conditions/pairs.py
+++ analysis/initial_conditions/pairs.py
@@ -1,10 +1,42 @@
 import numpy as np
 from n_T_balance import *
+from scipy import interpolate as interp
+from heating_cooling import cooling as cool
+from heating_cooling import heating as heat
 
-nmin = 1.0E-4
-nmax = 1.0
-n = np.logspace(np.log10(nmin), np.log10(nmax), 1.0E5)
-T= find_equilibrium(n,cooling_func=cool.sw_dm,
-                            heating_func=heat.metagalactic)
+nmin = 1.0E-10
+nmax = 100.0
+n = np.logspace(np.log10(nmin), np.log10(nmax), 1.0E4)
+T = find_equilibrium(n,cooling_func=cool.IIK_2007,
+                       heating_func=heat.IIK_2007,cf_kwargs={'mode':'modified'})
+#T= find_equilibrium(n,cooling_func=cool.sw_dm,
+#                            heating_func=heat.metagalactic)
 
-pressure_eq(n,T,n*T,filename='sw_dm_equilibrium_vals.dat')
+T = T[np.logical_not(np.isnan(T))]
+n = n[np.logical_not(np.isnan(T))]
+n = n[np.argsort(T)]
+T = T[np.argsort(T)]
+#print "--"
+#print np.min(T), np.max(T)
+#print T[0], T[1], T[-2], T[-1]
+#function = interp.UnivariateSpline(T,n*T)
+
+# find the pressure equillibrium for a few values of the corona
+P_spline = interp.UnivariateSpline(T,n*T)
+n_spline = interp.UnivariateSpline(T,n)
+
+Tsample = np.logspace(np.log10(np.min(T)),np.log10(np.max(T)),1.0E4)
+
+pressure_eq(n_spline(Tsample),Tsample,P_spline(Tsample),filename='sw_dm_equilibrium_vals.dat')
+
+plt.scatter(T,n)
+plt.plot(Tsample,n_spline(Tsample))
+#Tfun = np.logspace(np.log10(np.min(T)),np.log10(np.max(T)),1000)
+#vals = function(Tfun)
+
+#plt.plot(Tfun,vals,
+#           label='spline',lw=1.0,color='red')
+plt.loglog()
+#plt.xlim(1.0E2,1.0E8)
+plt.savefig('t.png')
+plt.close()
