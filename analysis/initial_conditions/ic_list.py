7f5e6744185c65a13428c2fc5046026e3bd14b6e
aemerick <aemerick11@gmail.com>
2015-02-14 21:21:01 -0500

    generalized IC list methods

--- analysis/initial_conditions/ic_list.py
+++ analysis/initial_conditions/ic_list.py
@@ -8,10 +8,104 @@ class dwarf_ic:
         self.name = name
 
     def set_ic(self, ic_dict):
+        """
+            Provided a dict containing the initial conditions, 
+            makes sure all the IC's are there and valid to generate
+            a legitimate initial condition. If not, attempts to solve
+            for them assuming the correct additional IC's are provided.
+
+            More info to come.....
+        """
+
         self.ic = ic_dict
 
         if not 'rho_crit' in self.ic.keys():
             self.ic['rho_crit'] = 9.74E-30 # z=0 crit. dens. of U
+
+
+        if not 'potential_type' in self.ic.keys():
+            print "MUST SET A 'potential_type'"
+            return
+
+        # check to make sure correct parameters are in dict
+        # require: 1) r_s 2) T 
+        if not 'b' in self.ic.keys() and not 'r_s' in self.ic.keys():
+            print "You must set the scale radius as 'b' or 'r_s'"
+        elif not 'b' in self.ic.keys():
+            self.ic['b'] = self.ic['r_s']
+        else:
+            self.ic['r_s'] = self.ic['b']
+
+        # if no mu's are set, assume them
+        if not 'mu_dwarf' in self.ic.keys():
+            print "Assuming primordial neutral for dwarf mu = 1.31"
+            self.ic['mu_dwarf'] = 1.31
+        if not 'mu_halo' in self.ic.keys():
+            print "Assuming primordial ionized for halo mu = 0.6"
+            self.ic['mu_halo'] = 0.6
+
+        
+        # M200 and n_o MUST BE SET!!!!! If not, try and solve for them
+        if not 'M200' in self.ic.keys() or not 'n_o' in self.ic.keys():
+            print "If M200 or n_o are not set, they will be solved for"
+            print "using the profile choice"
+
+            if not 'M_DM' in self.ic.keys() or\  
+               not 'M_HI' in self.ic.keys() or\
+               not 'r_DM' in self.ic.keys() or\
+               not 'r_HI' in self.ic.keys():
+                   print "MUST SET 'M_DM' 'M_HI' 'r_DM' 'r_HI' to solve"
+                   return
+
+            if 'T_halo' in self.ic.keys() and 'n_halo' in self.ic.keys():
+                print "ERROR: Both n_halo and T_halo canno be specified"
+                return
+            elif 'T_halo' in self.ic.keys():
+                T_halo = self.ic['T_halo']
+                n_halo = None
+            elif 'n_halo' in self.ic.keys():
+                n_halo = self.ic['n_halo']
+                T_halo = None
+            else:
+                print "ERROR: Either n_halo and T_halo must be specified"
+                return
+
+ 
+            if self.ic['potential_type'] == 'NFW':
+    return r_s, M200, n_o, T_halo, n_halo
+                
+
+                c, r_s, M200, n_o, T_halo, n_halo =\
+                               prof.solve_NFW(self.ic['M_DM'], self.ic['r_DM'],
+                               self.ic['r_s'] , self.ic['M_HI'],
+                               self.ic['r_HI'], self.ic['T'],
+                               mu=self.ic['mu_dwarf'],
+                               mu_halo=self.ic['mu_halo'],
+                               T_halo = T_halo, n_halo= n_halo,
+                               rho_crit = self.ic['rho_crit'])
+                self.ic['M200'] = M200
+                self.ic['n_o' ] = n_o
+                self.ic['T_halo'] = T_halo
+                self.ic['n_halo'] = n_halo
+                self.ic['c']      = c
+
+            elif self.ic['potential_type'] == 'Burkert':
+
+                r_s, M200, n_o, T_halo, n_halo=\
+                      prof.solve_Burkert(self.ic['M_DM'], self.ic['r_DM'],
+                               self.ic['r_s'] , self.ic['M_HI'],
+                               self.ic['r_HI'], self.ic['T'],
+                               mu_dwarf=self.ic['mu_dwarf'],
+                               mu_halo=self.ic['mu_halo']
+                               T_halo = T_halo, n_halo= n_halo,
+                               rho_crit = self.ic['rho_crit'])
+
+                self.ic['M200'] = M200
+                self.ic['n_o' ] = n_o
+                self.ic['T_halo'] = T_halo
+                self.ic['n_halo'] = n_halo
+
+
 
         self.Pcorona = self.ic['n_halo']*cgs.kb*self.ic['T_halo']
                                         
@@ -34,12 +128,28 @@ class dwarf_ic:
                            rho_crit = self.ic['rho_crit'],
                            Pcorona = self.Pcorona)
 
+
+        elif type == 'Burkert_isothermal':
+            rho, RM = prof.Burkert_isothermal_gas(r, r_s=self.ic['b'],
+                                 M200=self.ic['M200'], T=self.ic['T_dwarf'],
+                               n_o = self.ic['n_o'], mu = self.ic['mu_dwarf'],
+                               rho_crit=self.ic['rho_crit'])  
+
         self.rvals  = r
         self.rho    = rho
         self.radius = RM
 
-    
 
+    def FLASH_readable_ic(filename=None):
+        """
+           Saves to file or prints to screen the IC's
+           in a fashion that can be copy pasted into
+           the flash.par file.
+        """
+        
+        
+        print "currently does nothing"
+        return
 
 
 known_initial_conditions = {'Sextans_test':
