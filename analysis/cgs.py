42196009ba7d530169c64645f75a0964a1c96ea1
aemerick <aemerick11@gmail.com>
2015-02-24 13:57:04 -0500

    updated cgs values

--- analysis/cgs.py
+++ analysis/cgs.py
@@ -1,15 +1,17 @@
 # some useful CGS units we always need ...
 
-
+print "using cgs py"
 
 mu   = 0.6
-mp   = 1.67262178e-24 # mass of proton, grams
+mp   = 1.67262E-24
+#mp   = 1.67262178e-24 # mass of proton, grams
 kpc  = 3.08567758e21  # kiloparsec, cm
 pc   = kpc*1e-3
 Mpc  = kpc*1e3        # megaparsec, cm
 Msun = 1.9891e33      # mass of sun, grams
-G    = 6.674e-8       # Gravity, cgs
-kb   = 1.380658e-16		# Boltzmann, cgs
+G    = 6.67428e-8       # Gravity, cgs
+kb   = 1.38065E-16
+#kb   = 1.380658e-16		# Boltzmann, cgs
 yr   = 3.15569e7      # year in seconds
 Myr  = yr*1.0e6       # Megayear ""
 gyr  = yr*1.0e9       # gigayear ""
