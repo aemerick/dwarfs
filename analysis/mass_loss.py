54bfc37f62a76fd5185dee753f7358901456999f
aemerick <aemerick11@gmail.com>
2015-02-28 18:00:10 -0500

    example script for dwarf mass over time

--- analysis/mass_loss.py
+++ analysis/mass_loss.py
@@ -3,33 +3,41 @@ import matplotlib.pyplot as plt
 import yt
 import glob
 import numpy as np
-
+import cgs as cgs
 
 # operate on all sorted checkpoint files
-ds_names = glob.glob('*_chk_*')
+ds_names = glob.glob('*_plt_cnt_*')
 ds_names.sort() 
-#ds_names = [ds_names[0]] + [ds_names[10]] + [ds_names[100]] + [ds_names[1000]]
+
 m = np.zeros(np.size(ds_names))
 t = np.zeros(np.size(ds_names))
 
 
 # need a better way to do this..
 RM = None; i = 0
+f = open('mass_t.dat', 'w')
+f.write("#t m\n")
+
+sim = danal.simulation('dwarf_fullp_hdf5_plt_cnt_')
+RM = 360.3*cgs.pc
+
 for ds_name in ds_names:
     print ds_name
     ds = yt.load(ds_name)
 
     dw = danal.dwarf(ds, 'flash.par', rm=RM)
-    if RM == None:
-        RM = dw.rmatch(1000)
+  #  if RM == None:
+   #     RM = dw.rmatch(1000)
     
 
 
 
     m[i] = dw.param_contained('total_mass', r = RM).convert_to_units('Msun')
     t[i] = (dw.time).convert_to_units('Myr')
+    f.write("%8.8e %8.8e\n"%(t[i],m[i]))
     
     i = i + 1
+f.close()
 plt.plot(t,m/m[0],color='black',lw=1.5, label=r"M$_o$=%3.2E M$_{\odot}$"%(m[0]))
 plt.xlabel('t (Myr)')
 plt.ylabel(r'M(t)/M$_o$')
@@ -40,5 +48,5 @@ plt.xlim(t[0],t[-1])
 plt.savefig('plots/mass_t.png')
 plt.close()    
     
-np.savetxt('mass_t.dat', (t,m), fmt='%6.5e',header="#t m")
+#np.savetxt('mass_t.dat', (t,m), fmt='%6.5e',header="#t m")
     
