22c0ff6e309ddd2f6563cf31d6b2a32dff1e6680
aemerick <aemerick11@gmail.com>
2015-04-10 19:20:09 -0400

    completed revamp of model evolution. finished analytical_model class. tested RPS and KH

--- analysis/analytic_model/dwarf_model.py
+++ analysis/analytic_model/dwarf_model.py
@@ -5,6 +5,10 @@ import numpy as np
 import cgs as cgs
 from initial_conditions import profiles as prof
 
+from scipy import integrate
+from scipy import interpolate
+
+
 # given an orbit of a dwarf galaxy as a function of time
 #    - Given: r (position), v_dwarf, n_halo, T_halo 
 #    - return r_dwarf and M_gas as a function of that
@@ -24,26 +28,61 @@ from initial_conditions import profiles as prof
 
 class analytical_dwarf:
 
-    def __init__(self, name, ic, orbit, potential):
+    def __init__(self, name, ic):
         """ 
-            Supply ic's and orbital parameters
+            Initialize analytical dwarf model given a name and a dictionary of initial conditions
         """
 
         self.ic    = ic
-        self.orbit = orbit
+        
+        self.R_o   = self.ic['RM']
+        integrand = lambda x: x*x*self.gas_profile(x)
+        self.M_o   = 4.0*np.pi* integrate.quad(integrand, 0.0, self.R_o)[0]
 
-    def initialize_gas(self, R, potential):
-
-        if potential == 'NFW':
-            self.rho_o = prof.NFW_isothermal_gas(R, r_s=self.ic['b'],
-                                                 M200=self.ic['M200'],
-                                                 T = self.ic['T_dwarf'], 
-                                                 n_o = self.ic['n_o'],
-                                                 mu=self.ic['mu_dwarf'],
-                                                 rho_crit=self.ic['rho_crit'],
-                                      Pcorona = self.ic['n_halo']*cgs.kb*self.ic['T_halo'])
+        
+    def setup_orbit(self, t, halo_gas_density, galaxy_velocity):
+        """
+        Setup orbit parameters of dwarf galaxy... namely the halo gas density dwarf is
+        moving through over time, and the galaxy's velocity. Sets these up as 
+        functions of time using cubic spline interpolation.
+        
+        Parameters:
+        t  : array
+            Array of time values for orbit
+        halo_gas_density : float or array
+            Array of halo gas densities at the associated times. Requires mass density,
+            but makes assumptions to convert to mass density if number density is provided.
+            If a single float is provided, a constant function is made
+        galaxy_velocity : float or array
+            Array of galaxy velocities at the associated times. Makes function using
+            cubic spline interpolation. If float, a constant function is made
+            
+        Returns
+        -------
+        Nothing. Sets up 'halo_density' and 'galaxy_velocity' parameters.
+        """
+    
+        if any( [halo_gas_density > 1.0E-10] ) : # convert to mass density
+            halo_gas_density = halo_gas_density * self.ic['mu_halo'] * cgs.mp
+        
+        # if t is an array, then use a cubic spline to make a function from the orbital
+        # data. If t is a single value, then halo gas dnesity and velocity are constants..
+        # make them into functions anyway to make rest of everything work...
+        if np.size(halo_gas_density) > 1 : 
+            self.halo_density    = interpolate.UnivariateSpline(t, halo_gas_density,k=3)
+        else:
+            self.halo_density = lambda x: halo_gas_density
+        
+        if np.size(galaxy_velocity) > 1:
+            self.galaxy_velocity = interpolate.UnivariateSpline(t, galaxy_velocity ,k=3)
+        else:
+            self.galaxy_velocity = lambda x: galaxy_velocity
+   
 
     def gas_profile(self, R, potential='NFW'):
+        """
+        Evauluate the gas profile at given radii. Gas profile picked from profiles module
+        """
         
         if 'potential' in self.ic.keys():
             potential = self.ic['potential']
@@ -59,6 +98,9 @@ class analytical_dwarf:
         
         
     def DM_profile(self, R, potential='NFW'):
+        """
+        Evaluate dark matter profile at given radii. DM profile picked from profiles module        
+        """
         
         if 'potential' in self.ic.keys():
             potential = self.ic['potential']
@@ -67,14 +109,78 @@ class analytical_dwarf:
             return prof.NFW_DM(R, r_s=self.ic['b'], M200=self.ic['M200'], rho_crit=self.ic['rho_crit'],
                                decay=False, r_decay=None)
         
+        
+    def evolve(self, t, included_physics, physics_kwargs={}):
+        """
+        Wrapper around evolve_satellite function. Automatically supplies orbital params
+        and galaxy profile functions to evolve_satellite based upon the initial conditions 
+        already set up in the model.
+        
+        Parameters
+        -----------
+        t : array
+            Array of time values to evolve over
+        included_physics : list
+            See 'evolve_satellite' for more details. List of physics to include. Currently
+            'RPS' or 'KH'
+        physics_kwargs : dict
+            Dict of dicts to evolve satellite over.
+            
+        Returns
+        -------
+        M, R : array, array
+            Arrays of mass and radius evolution over time.
+        """
+        M, R = \
+                evolve_satellite(t, included_physics, self.halo_density, self.galaxy_velocity,
+                                              self.gas_profile, self.DM_profile, self.M_o, self.R_o,
+                                              physics_kwargs={})
+                
+        self.M = M
+        self.R = R
+        return M, R
 
-def evolve_satellite(t, included_physics, rho_halo, v, rho_gas, M_o, R_o, physics_kwargs={}):
+def evolve_satellite(t, included_physics, halo_gas_density, galaxy_velocity, galaxy_gas_density, rho_DM, M_o, R_o, physics_kwargs={}):
     """
     This function evolves the satellite given a list of desired physical processes
     to model for mass loss in the dwarf galaxy. The included_physics list is currently
     limited to 'RPS' and 'KH'. Physics kwargs should be a dictionary of kwargs with the
     kwargs for a given physics module assigned to the name of the physics, i.e.
     physics_kwargs = { 'RPS': kwargs_dict_for_RPS, 'KH': kwargs_dict_for_KH}.
+    
+    Parameters
+    t : array
+        Array of time values to calculate the ODE's over. 
+    included_physics: list
+        List of physics to include. Current options are:
+            'KH' - Kelvin Helmholtz instability.
+            'RPS' - Ram Pressure stripping. RPS mass loss rate calculated from
+                    rate at which its radius decreases over time, a function
+                    of the forward shock.
+    halo_gas_density : const or function
+        A function of time only that gives the halo gas density as the galaxy
+        moves through in its orbit. If a single value is given, assumed constant over
+        time
+    galaxy_velocity : float or function
+        A function of time only giving the galaxy velocity as it moves through its orbit.
+        If a single value is given, assumed constant over time
+    galaxy_gas_density : float or function
+        A function of radius from galaxy center only. Gives galaxy gas density as a 
+        function of r from center for a spherical dwarf galaxy model. If constant, assumed
+        to be a constant density.
+    rho_DM : float or function
+        A function of radius from galaxy center only. Gives galaxy dark matter density as a 
+        function of r from the center for a spherical dwarf galaxy model. If constnat, assumed
+        to be a constant density
+    M_o : float
+        Initial total gas mass of galaxy
+    R_o : float
+        Initial (gas) radius of galaxy
+        
+    Returns
+    -------
+    M, R: array
+        Arrays of gas mass and radius evolution of dwarf galaxy over time.
       
     """
     
@@ -83,15 +189,15 @@ def evolve_satellite(t, included_physics, rho_halo, v, rho_gas, M_o, R_o, physic
     
     # do a check of input parameters. Are they functions or constants?
     
-    if not hasattr(rho_halo, '__call__'):
-        halo_gas_density = lambda x : rho_halo
+    if not hasattr(halo_gas_density, '__call__'):
+        halo_gas_density = lambda x : halo_gas_density
         
-    if not hasattr(v, '__call__'):
-        galaxy_velocity = lambda x : v
+    if not hasattr(galaxy_velocity, '__call__'):
+        galaxy_velocity = lambda x : galaxy_velocity
         
     # galaxy gas density should be function of radius in galaxy!!!
-    if not hasattr(v, '__call__'):
-        galaxy_gas_density = lambda x : rho_gas # constant!
+    if not hasattr(galaxy_gas_density, '__call__'):
+        galaxy_gas_density = lambda x : galaxy_gas_density # constant!
     
 
     KH_const = 0.0; RPS_const = 0.0
@@ -103,42 +209,116 @@ def evolve_satellite(t, included_physics, rho_halo, v, rho_gas, M_o, R_o, physic
     
     if 'RPS' in included_physics:
         RPS_const = 1.0
+        
     if not 'RPS' in physics_kwargs.keys():
-        physics_kwargs['KH'] = {}
+        physics_kwargs['RPS'] = {}
     
     # need to come up with some way to make a function on the fly... constants is fine
     # but if this gets complicated then.... yaa.....
     
-    ode_function = lambda y, t :\
-                 KH_const *  _KH_evolution(y, t, halo_gas_density, galaxy_velocity,
+    ode_function = lambda y, t, A, B:\
+                 A *  _KH_evolution(y, t, halo_gas_density, galaxy_velocity,
                                                  galaxy_gas_density,physics_kwargs['KH'])+\
-                RPS_const * _RPS_evolution(y, t, halo_gas_density, galaxy_velocity,
-                                           galaxy_gas_density, galaxy_gas_density(0.0),physics_kwargs['RPS'])
-    # now solve!
-    soln = integrate.odeint(ode_function, [M_o, R_o], t)
+                 B * _RPS_evolution(y, t, halo_gas_density, galaxy_velocity,
+                                           galaxy_gas_density, galaxy_gas_density(0.0))
     
-    return soln
+    
+    # write a loop here... solve step by step
+    M = np.zeros(np.size(t)); R = np.zeros(np.size(t))
+    M[0] = M_o; R[0] = R_o
+    keep_looping = True; i = 0
+    while (i < np.size(t) - 1) and keep_looping:
+        
+        # check if ram pressure stripping occurs
+        if 'RPS' in included_physics:
+            # integrate and test around the current radius
+            rps_cond = _RPS_condition(np.linspace(0.9999*R[i],1.0001*R[i],5), rho_DM, galaxy_gas_density, 
+                                               halo_gas_density(t[i]), galaxy_velocity(t[i]))
+            
+            # if RPS is valid at current radius, use it... otherwise set to zero
+            if rps_cond[3] > 0:
+                RPS_const = 1.0
+            else:
+                RPS_const = 0.0 
+        
+        soln = integrate.odeint(ode_function, [M[i],R[i]], t[i:i+2], args=(KH_const,RPS_const,) )
+        M[i+1] = soln[1,0]; R[i+1] = soln[1,1]
+        
+        i = i + 1
+        if M[i] <= 0 or R[i] <=0:
+            M[i] = 0.0; R[i] = 0.0
+            keep_looping = False
+            
+    
+    return M, R
 
+def _RPS_condition(r, DM_density, gas_density, halo_density, galaxy_velocity, alpha=2.0):
+    # need DM density profile, gas density profile, and 
+    
+    # find the mass profile from density profile
+    DM_integrand = lambda x: x*x*DM_density(x)
+    gas_integrand = lambda x: x*x*gas_density(x)
+    M_DM      = np.zeros(np.size(r)-1)
+    M_gas     = np.zeros(np.size(r)-1)
+    
+    for i in np.arange(1,np.size(r)):
+        M_DM[i-1] = 4.0*np.pi* integrate.quad(DM_integrand, 0.0, r[i])[0]
+        M_gas[i-1] = 4.0*np.pi* integrate.quad(gas_integrand, 0.0,r[i])[0]
+    
+    # Now, calculate the RHS and LHS of the stripping condition
+    RHS = (M_DM + M_gas) * gas_density(r[:-1]) / r[:-1] * cgs.G * beta
+    LHS = halo_density * galaxy_velocity * galaxy_velocity
+    
+    return LHS - RHS
+    
 
-def _KH_evolution(y, t, halo_gas_density, galaxy_velocity, galaxy_gas_density):
+def _KH_evolution(y, t, halo_gas_density, galaxy_velocity, galaxy_gas_density, *args, **kwargs):
 
     Mi = y[0]
     Ri = y[1]
     
     num = 1.0
     if Mi <= 0 or Ri <=0:
-        Ri = 0.0
-        num = 0.0
+        Mdot = 0.;Rdot=0.
+    else:
+        Mdot = -np.pi * Ri**2 * halo_gas_density(t) * galaxy_velocity(t)
+        Rdot = Mdot / (4.0*np.pi*Ri**2 * galaxy_gas_density(Ri))
         
-    Mdot = -np.pi * Ri**2 * halo_gas_denisty(t) * galaxy_velocity(t)
-    
-    Rdot = Mdot / (4.0*np.pi*Ri**2 * galaxy_gas_density(Ri))
-        
-    return [Mdot, Rdot]
+    return np.array([Mdot, Rdot])
 
-def RPS_evolution(y, t, halo_gas_density, galaxy_velocity, galaxy_gas_density, rho_gas_o,
-                  method='shock', T_galaxy = None, mu_galaxy = None):
+def _RPS_evolution(y, t, halo_gas_density, galaxy_velocity, galaxy_gas_density, rho_gas_o,
+                  method='shock', T_galaxy = None, mu_galaxy = None, *args, **kwargs):
     """
+        This function can be supplied to an ode solver (taken care of by the 
+        evolve_satellite function) to model mass loss and radius change of a 
+        spherical galaxy undergoing ram pressure stripping. RPS mass loss
+        rate is calculated in two methods, set by the 'method' kwarg.
+        
+        Parameters
+        -----------
+        y   :  2 element array or list
+            Two element array or list with the first element containing mass 
+            and the second radius
+        t   : array
+            Dependent variable for ODE. Time in cgs
+        halo_gas_density : function
+            Function of time only that gives the gas density of the halo
+            the galaxy is moving through.
+        galaxy_velocity: function
+            Function of time only that gives the velocity of the galaxy
+        galaxy_gas_density: function
+            Function of R only. The spherically symettric gas (mass) density
+            profile of the dwarf galaxy. 
+        rho_gas_o : real
+            Value of the above at R = 0 (central gas density).
+        
+        Returns
+        -------
+        [Mdot, Rdot] : List 
+            
+        
+        
+    
         TO DO: make a kwarg to switch between mdot methods...
                this one, nichols, and then using the sound speed scaling 
                
@@ -150,22 +330,21 @@ def RPS_evolution(y, t, halo_gas_density, galaxy_velocity, galaxy_gas_density, r
        
     num = 1.0
     if Mi <= 0 or Ri <=0:
-        Ri = 0.0
-        num = 0.0
-    
-    if method == 'shock':
-        # using the shock speed to give the RPS timescale
-        # taking mdot as current mass divided by timescale
-        vshock = (4./3.)*(halo_gas_density(t)/rho_gas_o)**0.5 * galaxy_velocity(t)
-        tau_rps = 2.0*Ri/ vshock
-        mdot = -Mi / tau_rps      
-        rdot = mdot / (4.0*np.pi*Ri**2 * galaxy_gas_density(Ri))
-    elif method == 'sound':
-        c_sound = ((5.0/3.0)*cgs.kb*T_galaxy/(cgs.mp*mu_galaxy))**0.5
-        rdot = -c_sound
-        mdot = 4.0*np.pi*Ri**2 * galaxy_gas_density(Ri) * rdot
+        mdot=0.; rdot=0.
+    else:
+        if method == 'shock':
+            # using the shock speed to give the RPS timescale
+            # taking mdot as current mass divided by timescale
+            vshock = (4./3.)*(halo_gas_density(t)/rho_gas_o)**0.5 * galaxy_velocity(t)
+            tau_rps = 2.0*Ri/ vshock
+            mdot = -Mi / tau_rps      
+            rdot = mdot / (4.0*np.pi*Ri**2 * galaxy_gas_density(Ri))
+        elif method == 'sound':
+            c_sound = ((5.0/3.0)*cgs.kb*T_galaxy/(cgs.mp*mu_galaxy))**0.5
+            rdot = -c_sound
+            mdot = 4.0*np.pi*Ri**2 * galaxy_gas_density(Ri) * rdot
    
-    return [mdot, rdot]
+    return np.array([mdot, rdot])
 
 
 
