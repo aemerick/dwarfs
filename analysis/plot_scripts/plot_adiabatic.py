1b6318b008ffe66ddb910ea8a3020a5b5339c006
aemerick <aemerick11@gmail.com>
2015-07-24 18:11:02 -0400

    first commit

---
+++ analysis/plot_scripts/plot_adiabatic.py
@@ -0,0 +1,118 @@
+import numpy as np
+import yt
+import dwarf as dw
+from initial_conditions import ic_list as icl
+import os
+
+import matplotlib.pyplot as plt
+from   matplotlib    import rc
+
+fsize = 15
+rc('text', usetex=True)
+rc('font', size=fsize)#, ftype=42)
+line_width = 2.5
+
+
+work_dir = '/home/emerick/Research/dwarfs/flash_runs/leo_T'
+base_name = 'dwarf_fullp_hdf5'
+figure_name = 'LT_adiabatic_mass_evolution.png'
+
+mass_file_ext = '_cont_mass_1-0.dat'
+
+# load known subdirectories
+sub_dirs = next(os.walk(work_dir))[1]
+
+sim_names = [s for s in sub_dirs if 'LT' in s and not 'radbox' in s]
+sim_dirs  = [work_dir + '/' + s for s in sim_names]
+
+mass_files = [s + '/' + name + mass_file_ext for s,name in zip(sim_dirs,sim_names)]
+
+# set up the panel plot
+#
+# make it a 3 row and 2 column plot
+#
+#
+#
+#
+fig, ax = plt.subplots(3,2) 
+
+n020_loc = 0 ; v2_loc = 0 ; nh3_color = 'blue'
+n075_loc = 1 ; v4_loc = 1 ; nh4_color = 'green'
+n150_loc = 2 ;              nh5_color = 'red'
+
+nh3_label = r'n$_{\rm{halo}}$ = 10$^{%1i}$ cm$^{-3}$'%(-3)
+nh4_label = r'n$_{\rm{halo}}$ = 10$^{%1i}$ cm$^{-3}$'%(-4)
+nh5_label = r'n$_{\rm{halo}}$ = 10$^{%1i}$ cm$^{-3}$'%(-5)
+
+
+xlabel = r'Time (Myr)'
+ylabel = r'M$_{\rm{cold,gas}}$ / M$_{\rm{o}}$'
+
+xlim = (0.0, 2000.0)
+ylim = (0.0,    1.0)
+
+for i in np.arange(len(sim_names)):
+    name = sim_names[i]
+    sim_dir = sim_dirs[i]
+
+    if 'n020' in name:
+        plot_row = n020_loc ; model_label = r'n$_{\rm{o}}$ = %.2f'%(0.02)
+    elif 'n075' in name:
+        plot_row = n075_loc ; model_label = r'n$_{\rm{o}}$ = %.2f'%(0.75)
+    else:
+        plot_row = n150_loc;  model_label = r'n$_{\rm{o}}$ = %.2f'%(1.50)
+    if 'v2' in name:
+        plot_column = v2_loc
+    else:
+        plot_column = v4_loc
+    if 'nh3' in name:
+        color = nh3_color
+        label = nh3_label 
+    elif 'nh4' in name:
+        color = nh4_color
+        label = nh4_label #r'n$_{\rm{halo}}$ = 10$^{%1i}$ cm$^{-3}$'%(-4)
+
+    else:
+        color = nh5_color
+        label = nh5_label #r'n$_{\rm{halo}}$ = 10$^{%1i}$ cm$^{-3}$'%(-5)
+    
+
+    plot_loc = (plot_row, plot_column)
+
+    mass_file = mass_files[i]
+
+    if os.path.isfile(mass_file):
+
+        try:
+            data = np.genfromtxt(mass_file,names=True)
+            ax[plot_loc].plot(data['t'] , data['m']/data['m'][0], color = color,
+                                          lw = line_width, ls = '-', label=label)
+
+            axis = ax[plot_loc]
+            axis.annotate(model_label,xy=(1400,0.8),xytext=(1400,0.8),textcoords='data')
+
+        except:
+            print 'nothing'        
+    else:
+        print 'Mass evolution file does not exist for ' + name
+
+    ax[plot_loc].set_xlim(xlim)
+    ax[plot_loc].set_ylim(ylim)
+    ax[plot_loc].set_xlabel(xlabel)
+    ax[plot_loc].set_ylabel(ylabel)
+
+
+ax[(0,v2_loc)].set_title(r'v$_{\rm{gal}}$ = %3i km s$^{-1}$'%(200.0))
+ax[(0,v4_loc)].set_title(r'v$_{\rm{gal}}$ = %3i km s$^{-1}$'%(400.0))
+
+
+
+#ax[0,0].plot(-1,-1,color=nh3_color,label=nh3_label, ls='-', lw=line_width)
+#ax[0,0].plot(-1,-1,color=nh4_color,label=nh4_label, ls='-', lw=line_width)
+#ax[0,0].plot(-1,-1,color=nh5_color,label=nh5_label, ls='-', lw=line_width)
+#ax[(0,0)].legend(loc='best',fancybox=True)
+#ax[0,0].legend(loc='upper center', bbox_to_anchor=(0.5, 1.05),
+#          ncol=3, fancybox=True, shadow=True)
+#fig.save()
+plt.tight_layout()
+fig.savefig(figure_name)
---
+++ analysis/plot_scripts/plot_model_example.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import cgs as cgs
+from analytic_model import dwarf_model as dw_model
+from initial_conditions import ic_list as icl ;
+from matplotlib import rc
+
+line_width = 2.5
+
+#
+simulation_dir = '/home/emerick/Research/dwarfs/flash_runs/leo_T/'
+model_name = "LT_n075_v4_nh4"
+sim_mass   = model_name + '_cont_mass_1-0.dat'
+
+
+#
+initial_conditions = icl.ic_object_dict[model_name]
+anal_model = dw_model.analytical_dwarf(model_name, initial_conditions.ic)
+anal_model.load_simulation_mass(simulation_dir +model_name +'/' + sim_mass)
+anal_model.setup_orbit(0.0, initial_conditions.ic['n_halo'], initial_conditions.ic['v_halo'])
+
+
+tmax = anal_model.simulation_data['time'][anal_model.simulation_data['mass'] == 0.0][5]
+
+# desired timestep size and start / end times... in Myr
+dt = 0.5
+tmin = 0.0
+npoints = np.ceil((tmax - tmin)/dt)
+
+t = np.linspace(tmin, tmax, npoints)*cgs.Myr
+
+shock_kwargs = {'RPS':{'alpha': 1.475,'beta': 0.800,'method':'shock'},'KH':{'beta': 5.}}
+
+# do the full plot
+M , R = anal_model.evolve(t, ['RPS','KH'], RPS_KH_exclusive = True, physics_kwargs=shock_kwargs)
+plt.plot(anal_model.simulation_data['time'], anal_model.simulation_data['mass']/anal_model.simulation_data['mass'][0], label='Simulation', color='black',ls='-',lw=line_width)
+plt.plot(t/cgs.Myr, M/M[0], label = 'Analytic - RPS + KH', color='red',ls='-',lw=line_width)
+
+# just RPS
+M , R = anal_model.evolve(t, ['RPS'], RPS_KH_exclusive = True, physics_kwargs=shock_kwargs)
+plt.plot(t/cgs.Myr, M/M[0], label = 'Analytic - RPS', color='red',ls=':',lw=line_width)
+
+# just KH
+M , R = anal_model.evolve(t, ['KH'], physics_kwargs=shock_kwargs)
+plt.plot(t/cgs.Myr, M/M[0], label = 'Analytic - KH', color='red',ls='--',lw=line_width)
+
+plt.xlim(0,tmax)
+plt.ylim(0,1)
+
+plt.legend(loc='best',fancybox=True)
+
+plt.savefig('LT_example_adiabatic_fit.png')
+
+plt.close()
