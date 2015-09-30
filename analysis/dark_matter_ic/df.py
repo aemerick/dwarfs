d1a41c8b8974e9289444a1885668c0bac81893e3
aemerick <aemerick11@gmail.com>
2015-09-30 12:02:42 -0400

    Finalized functions. Added load df from file. Added comments and function docs

--- analysis/dark_matter_ic/df.py
+++ analysis/dark_matter_ic/df.py
@@ -3,35 +3,81 @@ from __future__ import division
 import numpy as np
 
 # for numerical derivation and integration
-from scipy.misc import derivative
-from scipy import integrate
-from scipy import optimize
+from scipy      import integrate    # for quad integration
+from scipy      import optimize     # for root finder
 
 
 # constants in cgs units and unit conversions
 import cgs as cgs
 
-# likely not necessary to import
-import dm_density_profiles as dm_prof
 
-
-#
-# Phi and Psi are not the same !!!!!!!!!!!!!!!!!
-#
 
 class DF:
 
     def __init__(self, dprof):
-
+        """
+        Initialize the DF class by passing a dark matter density profile object
+        
+        Parameters
+        ----------
+        dprof : general_dm_profile object
+            Dark matter profile object from dm_density_profiles.py
+        """
+        
         self.dprof = dprof
-
-        self.max_E = self.relative_potential(0.0)    
-
-    def compute(self, n_points, M_DM, N_DM,  E = None):
+        
+    def load_df(self, filename):
         """
-        Tabulates the distribution function
+        Loads f(E) from a file. File assumed to have header names of "E" and "f"
+        
+        Parameters
+        ----------
+        filename : string
+            Path of file to read from
         """
-        self.M_DM = M_DM ; self.N_DM = N_DM
+
+        try:
+            data = np.genfromtxt(filename, names=True)
+        
+            self.E = data['E']
+            self.f = data['f']
+            
+            npoints = np.size(self.f)
+            print "%4i DF points successfuly loaded from "%(npoints) + filename
+            
+        except:
+            print "Error in loading from file. Check header names. Should be E and f"
+        
+        
+        
+        return
+        
+        
+    def compute(self, n_points, verbose = True, E = None, filename = None):
+        """
+        Tabulates the distribution function. DF is computed as given in BT XXXX.
+        Energy values and f(E) are stored as parameters of the object.
+        
+        Parameters
+        ----------
+        n_points : int
+            Number of points to compute for the distribution function. The energy range
+            is automatically computed over the minimum / maximum energy values possible as determined
+            by the 'small_r' and 'large_r' parameters from the dm density class. This is a numerically
+            tractable way of integrating from Psi = 0 to Psi = E, as the former occurs as r -> inf and
+            latter only at r = 0
+        verbose : boolean, optional
+            Turns on/off verbose output. If off, nothing is printed. Default : True
+        E : ndarray, optional
+            Energy values to compute DF (i.e. f(E) ). This overrides the automatic calculation of E. 
+            Be careful with this. Default : None
+        filename : string, optional
+            If filename is provided, writes out E and f(E) to a text file. Default : None
+            
+         Returns;
+         f : ndarray
+            Distribution function as computed at energy values E. 
+        """
 
         # if E is not specified, choose it 
         #
@@ -41,10 +87,8 @@ class DF:
 
             E = np.logspace( np.log10(E_min), np.log10(E_max), n_points)
 
-            # now normalize to energy per unit mass
-           # E = E / self.M_DM
-
-        # integrand:
+           
+        # integrand in the DF integral
         def _integrand(x, E_val):
 
             r_val = self._r_root_finder(x)
@@ -75,27 +119,34 @@ class DF:
 
         i = 0; f_prev = 0.0
         for E_value in E:
-            print "%03i Computing value for E = %.3E"%(i,E_value),
+            if verbose:
+                print "%03i Computing value for E = %.3E"%(i,E_value),
             self.f[i] = integrate.quad(_integrand, lower_bound, E_value, args=(E_value,))[0] #+ f_prev
 
 
             self.f[i] = self.f[i] + 1.0/np.sqrt(E_value) * (self.dprof.first_derivative(0.99*self.dprof.large_r))*(1.0/(self._dPsi_dr(0.99*self.dprof.large_r)))
 
-            print " - f = %0.3E"%(self.f[i])
-            #f_prev = self.f[i] ; lower_bound = E_value
+            if verbose:
+                print " - f = %0.3E"%(self.f[i])
+
             i = i + 1
 
-        # multiply by the constants out front
-
-
-        #normalization = self.M_DM**(1.0/2.0) / self.N_DM
-        #normalization = self.dprof.M_vir**(1.0/2.0)
+        # multiply by the constants and normalize
         normalization = 1.0/self.dprof.M_sys
 
         self.f = self.f * normalization / (np.sqrt(8.0) * np.pi*np.pi)
 
         self.E = E
 
+        # write out if filename is given
+        if (not filename == None):
+            f = open(df_filename,'w')
+            f.write("# E f\n")
+            for i in np.arange(np.size(self.f)):
+                f.write("%.8E %.8E\n"%(self.E[i],self.f[i]))
+            f.close()
+        
+        
         if scalar_input:
             return np.squeeze(self.f)
         else:
@@ -106,10 +157,7 @@ class DF:
         
     def _d2rho_dPsi2(self,r):
         """
-        Computes the second derivative of density with respect to potential using the chain rule
-
-        NEED TO FIX FOR THE WHOLE RELATIVE POTENTIAL CRAP... make sure the sign is correct
-
+        Computes the second derivative of density with respect to relative potential using the chain rule
         """
 
         dr_dPsi = 1.0 / self._dPsi_dr(r)
@@ -124,7 +172,7 @@ class DF:
     def _r_root_finder(self, psi_value):
 
         """ 
-        Solves the equation   phi_value - dprof.phi(r) = 0.0 for r
+        Solves the equation phi_value - dprof.phi(r) = 0.0 for r 
         """
         def _eq_to_solve(x, pval):
             return pval - self.relative_potential(x)
@@ -166,14 +214,14 @@ class DF:
 
     def _dPsi_dr(self, r):
         """
-        Derivative of relative potential with respect to r... Psi = - phi
+        Derivative of relative potential with respect to r... Again, Psi = - phi
         """
         return -1.0 * self.dprof.dPhi_dr(r)
 
 
     def _d2Psi_dr2(self, r):
         """
-        Second derivative of relative potential with respect to r... Psi = - phi
+        Second derivative of relative potential with respect to r... Again, Psi = - phi
         """
         return -1.0 * self.dprof.d2Phi_dr2(r)
 
@@ -183,7 +231,23 @@ class DF:
 
 def hernquist_df(E, M, a):
     """
-    Computes the Hernquist distribution function, whose solution is known analytically.
+    Computes the Hernquist distribution function analytically. This is used for testing purposes. Should 
+    get errors of a few percent with at most 8-10% closer to the maximum energy value (i.e. r -> 0 in the 
+    potential).
+    
+    Paramters
+    ---------
+    E : ndarray
+        Energy values to evaluate f(E)
+    M : float
+        System mass
+    a : float
+        System scale radius
+        
+    Returns
+    ---------
+    f : ndarray
+        f(E), as evaluated at E
     """
 
     # scale E
