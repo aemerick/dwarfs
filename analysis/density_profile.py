50fea992bf6dd6c8f7e55345735924c39d7122cd
aemerick <aemerick11@gmail.com>
2017-04-25 11:06:17 -0400

    substantial plot modifications

--- analysis/density_profile.py
+++ analysis/density_profile.py
@@ -5,21 +5,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # simulation directory
-work_dir = '/home/emerick/Research/dwarfs/flash_runs/leo_T/LT_n075_v2_nh5_radbox/star/'
+work_dir = '/home/emerick/Research/dwarfs/flash_runs/leo_T/LT_n075_v2_nh4_radtunnel/'
 base_name = 'dwarf_fullp_hdf5'
 
 nbins      = 30
-field_list         = ['temperature']#['density']#,'temperature','pressure']
+field_list         = ['density','temperature','cell_mass']#['density']#,'temperature','pressure']
 #field_ylims[field] = {'density' : , 'temperature': (1.0E3,1.0E6), 'pressure':} 
 
 sim = dw.simulation(base_name, ds_dir = work_dir)              
 
 xlim = (0.0, 600.0)
     
-field_label = {'density' : r'Density (g cm$^{-3}$)', 'temperature': r'Temperature (K)'}
+field_label = {'density' : r'Density (g cm$^{-3}$)', 'temperature': r'Temperature (K)', 'cell_mass' : r'Mass (M$_{\odot}$)'}
+               
+field_units = {'density' : 'g*cm**(-3)', 'temperature': 'K', 'cell_mass': 'Msun'}
 xlabel = r'Radius (pc)'
 for field in field_list:
-    dw.profile_1D(sim, 'radius', field, xlim*yt.units.pc, nbins) # compute the profile
+    
+    if field == 'cell_mass':
+        accumulation=True ; weight_field = None
+    else:
+        accumulation=False; weight_field = 'cell_volume'
+        
+    
+    dw.profile_1D(sim, 'radius', field, xlim*yt.units.pc, nbins, accumulation=accumulation, weight_field=weight_field) # compute the profile
 
     
     profile = sim.profiles['radius'][field]
@@ -27,13 +36,26 @@ for field in field_list:
     t       = sim.profile_times#.convert_to_units('Myr').value
  
     # find max and mind values
-    minval = np.min(np.array(profile)) ; maxval = np.max(np.array(profile))
+    #minval = np.min(np.array(profile)) ; maxval = np.max(np.array(profile))
+    
+    maxval = np.min(profile[0].convert_to_units(field_units[field]).value)
+    minval = np.max(profile[0].convert_to_units(field_units[field]).value)
+
+    for p in profile:
+        tempmin = np.min(p.convert_to_units(field_units[field]).value)
+        tempmax = np.max(p.convert_to_units(field_units[field]).value)
+
+        if minval > tempmin:
+            minval = tempmin
+        if maxval < tempmax:
+            maxval = tempmax
+            
     if field == 'temperature': 
         minval = 100.0 ; maxval = 6.0E7
 
     for i in np.arange(np.size(t)):
         
-        plt.plot(r, profile[i].value, lw=2.5)
+        plt.plot(r, profile[i].convert_to_units(field_units[field]).value, lw=2.5)
         plt.semilogy()
         
         plt.ylabel(field_label[field])
