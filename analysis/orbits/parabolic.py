9c1390b5f7bb28d1f2f956b3556967ea09e56826
aemerick <aemerick11@gmail.com>
2017-06-15 14:42:31 -0400

    cleaning up generation and file format. plots

--- analysis/orbits/parabolic.py
+++ analysis/orbits/parabolic.py
@@ -9,9 +9,9 @@ from astropy import constants as const
 
 class parabolic:
 
-    def __init__(self, M_v = 1.0E10*u.Msun, M_p = 1.0E11*u.Msun,
-                       b   = 20.0*u.kpc, t_o = -1000*u.Myr,
-                       alpha = np.pi/6.0, dt = 0.5*u.Myr,
+    def __init__(self, M_v = 2.0E10*u.Msun, M_p = 5.32E10*u.Msun,
+                       b   = 20.0*u.kpc, t_o = -1250*u.Myr,
+                       alpha = 0.0, dt = 2.0*u.Myr,
                        t_final = None):
 
         self.M_v = M_v
@@ -101,15 +101,30 @@ class parabolic:
 
         f = open('parabolic_orbit.txt', 'w')
 
-        f.write("#t x y z\n")
+        f.write("# Orbital Properties:\n")
+
+        m_v = self.M_v.to('Msun').value
+        m_p = self.M_p.to('Msun').value
+        b   = self.b.to('kpc').value
+        alpha = self.alpha * 180 / (np.pi)
+        v_o   = (self.v_o()).to('km/s').value
+
+        f.write("# M_v = %4.4E Msun  -  M_p = %4.4E Msun\n"%(m_v, m_p))
+        f.write("# b = %4.4E kpc  -  alpha = %4.4E deg-  v_o = %4.4E km/s \n"%(b, alpha, v_o))
+        f.write("# --------------------------------------------------------------------- \n")
+        f.write("# t x y z\n")
 
         t = self.t_o
+
+        norm = -1.0 * np.sign(self.t_o.value) * np.abs(self.t_o)
 
         while t < self.t_final:
             x, y, z = self.pos(t, cartesian=True)
             x = x.value; y = y.value; z = z.value
 
-            f.write("%5.5E %8.8E %8.8E %8.8E\n"%(t.value, x, y, z))
+            t_write = t + norm
+
+            f.write("%5.5E %8.8E %8.8E %8.8E\n"%(t_write.value, x, y, z))
             t = t + self.dt
 
         f.close()
@@ -133,7 +148,7 @@ if __name__ == "__main__":
 
     orbit.save_orbit()
 
-    data = np.genfromtxt('parabolic_orbit.txt', names = True)
+    data = np.genfromtxt('parabolic_orbit.txt', names = True, skip_header = 4)
 
 
     fig = plt.figure()
@@ -142,7 +157,7 @@ if __name__ == "__main__":
 
     ax.scatter(data['x'], data['y'], data['z'],  c = plt.cm.viridis(data['t']/np.max(data['t'])))
     
-    npoints = 1.0E5
+    npoints = 1.0E3
     r = np.random.rand(npoints) * 10
     theta = np.random.rand(npoints) * 2.0 * np.pi
 
@@ -151,6 +166,17 @@ if __name__ == "__main__":
 
     ax.set_xlabel('x'); ax.set_ylabel('y'); ax.set_zlabel('z')
     fig.savefig('parabolic_orbit.png')
-    plt.show()
+
+    i = 0
+    for x in [90, 180, 270]:
+        ax.view_init(elev = 0.0, azim = x)
+        fig.savefig('parabolic_orbit_%i.png'%(i))
+        i = i + 1
+
+    for x in [45, 90, 135, 180]:
+        ax.view_init(elev = x, azim = 0.0)
+        fig.savefig('parabolic_orbit_%i.png'%(i))
+        i = i + 1
+
 	
 
